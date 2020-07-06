fa4a43389d6ef5ef36a3734af17852be04555959
Willta <4a039063@stust.edu.tw>
2020-07-07 00:48:42 +0800

    reupdate

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     publicPath: '/BT21-Demo/dist/',
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/repoName/'
-        : '/',
+    // publicPath: process.env.NODE_ENV === 'production'
+    //     ? '/repoName/'
+    //     : '/BT21-Demo/dist/',
     css: {
         extract: false
     }    
